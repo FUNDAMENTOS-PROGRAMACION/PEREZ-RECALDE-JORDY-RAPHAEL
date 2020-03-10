7110ab1b4f963528d2b22684fe9a78b511cebf7f
raphael899 <61747900+raphael899@users.noreply.github.com>
2020-03-10 11:54:52 -0500

    jaja
    suma

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -15,20 +15,28 @@ double numeroDouble = 0.0;
 char letras = 'u';
 String cadena ="hola mundo";
 boolean decsion = false;
-valorBoo1= true;
+//valorBoo1= true;//
 boolean decsion1 = false;
-
+public static void imprimirNombre(){
+System.out.println("Rapahel");
+        }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        imprimirNombre();
+        sumar2Nummeros(3 ,5);
+
         // TODO code application logic here
     }
+    public static int sumar2Nummeros (int num1 , int num2){
+        int suma = num1 + num2;
+        return suma ;
+    }
     
-}
-//aqui estuvo axel roses
-/*el cristhian le toco el ganzo aal jhony*/
 
+//aqui estuvo axel roses
+}
   
 
 
